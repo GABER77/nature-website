3a6160809cfebc1b8111f73ca754d2a8ff139b5e
GABER77 <mg7965724@gmail.com>
2025-03-13 15:38:57 +0200

    Prepare for deployment

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/globalErrorHandler');
@@ -103,6 +104,9 @@ app.use(
     ],
   }),
 );
+
+// Enable compression to speed up response delivery
+app.use(compression());
 
 // Test middleware
 app.use((req, res, next) => {
--- models/tourModel.js
+++ models/tourModel.js
@@ -162,11 +162,6 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
-tourSchema.post(/^find/, function (docs, next) {
-  console.log(`Query took ${Date.now() - this.start} milliseconds`);
-  next();
-});
-
 // Aggregation Middleware
 tourSchema.pre('aggregate', function (next) {
   const firstStage = this.pipeline()[0];
